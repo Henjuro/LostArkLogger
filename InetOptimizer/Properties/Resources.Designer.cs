1a9d843b8eb0d4e1efa6f7dc2565472cc063ce70
Henjuro <104998825+Henjuro@users.noreply.github.com>
2022-09-01 14:01:59 +0200

    Rename to InetOptimizer

--- InetOptimizer/Properties/Resources.Designer.cs
+++ InetOptimizer/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace LostArkLogger.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LostArkLogger.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("InetOptimizer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
